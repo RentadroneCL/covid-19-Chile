62bc4f63c5f6633b3dd4311b8b5b2abd59671e7b
Alvaro Farias <alvaro.farias@protonmail.com>
2020-03-30 20:00:48 -0300

    🚦 new endpoint

--- routes/api.py
+++ routes/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.Total import Total
 from app.DailyReport import DailyReport
+from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
@@ -11,8 +12,28 @@ async def index():
 
     return data
 
+
 @router.get('/total')
 async def total():
     data = Total.all()
 
     return data.last()
+
+
+@router.get('/totals')
+async def totals():
+    data = Total.all()
+
+    # data[0]._original['new_cases']
+    # data[0].new_cases
+
+    collect = {
+        'new_cases': data.map(lambda item: item.new_cases),
+        'total_cases': data.map(lambda item: item.total_cases),
+        'deceased': data.map(lambda item: item.deceased),
+        'recovered': data.map(lambda item: item.recovered),
+        'last_update': data.map(lambda item: item.last_update)
+    }
+
+    return collect
+
